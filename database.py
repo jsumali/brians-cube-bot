0c4f25d00edd96ae344ded793e28fb62254dfac4
Josh Sumali <j.sumali@gmail.com>
2020-04-06 20:12:37 -0400

    Squashed commit of the following:
    commit a20c6c693ff46263a662f00fa1738f914275b90c
    Author: Josh Sumali <j.sumali@gmail.com>
    Date:   Mon Apr 6 20:12:17 2020 -0400
    
        updates
    
    commit ae7c27c1f338157b4dddd6b6c4e481c1591dfa5b
    Author: Josh Sumali <j.sumali@gmail.com>
    Date:   Tue Feb 4 21:02:16 2020 -0500
    
        fix formatting, duplicate dates
    
    commit 4c7cc0c1d0c2bc5c1b2eba3b1d33e71fd5dd8b40
    Author: Josh Sumali <j.sumali@gmail.com>
    Date:   Tue Feb 4 20:51:18 2020 -0500
    
        a few more events
    
    commit 9f42264bba8f9df3e1f27a9ab0cbfe47c4a6f8a3
    Author: Josh Sumali <j.sumali@gmail.com>
    Date:   Sun Jan 26 15:45:15 2020 -0500
    
        fix duplicate ctx.send
    
    commit 78e4bfa0e87f6b37c60db115bbf9bb75d01597e9
    Author: Josh Sumali <j.sumali@gmail.com>
    Date:   Sun Jan 26 15:41:30 2020 -0500
    
        working database implementation
    
    commit edcebbae9d5cdec8b367f473f6847834cea742e5
    Author: Josh Sumali <j.sumali@gmail.com>
    Date:   Sat Jan 25 18:02:41 2020 -0500
    
        remove oracle text
    
    commit 7ebb188e486390286f747deec77ad239dcddbdd7
    Author: Josh Sumali <j.sumali@gmail.com>
    Date:   Fri Jan 24 22:36:17 2020 -0500
    
        add todo, data files

---
+++ database.py
@@ -0,0 +1,153 @@
+import sqlite3
+
+import scrython
+
+conn = sqlite3.connect('cube.db')
+c = conn.cursor()
+
+
+def init():
+    c.execute(
+        '''
+        CREATE TABLE event (
+            id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            event_date text, 
+            winner text, 
+            deckname text
+        )
+        '''
+    )
+    c.execute(
+        '''
+        CREATE TABLE event_decklist (
+            id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            event_date text, 
+            card_qty integer, 
+            card_name text, 
+            card_type text, 
+            card_mana_cost text
+        )
+        '''
+    )
+    c.execute(
+        '''
+        CREATE TABLE event_match_results (
+            id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            event_date text, 
+            round integer, 
+            player_1 text, 
+            player_2 text, 
+            player_1_wins integer, 
+            player_2_wins integer, 
+            draws integer
+        )
+        '''
+    )
+
+
+def type_line_to_type(type_line):
+    type_line = type_line.lower()
+
+    if "creature" in type_line:
+        return "Creatures"
+    if "planeswalker" in type_line:
+        return "Planeswalkers"
+    if "instant" in type_line or "sorcery" in type_line:
+        return "Spells"
+    if "artifact" in type_line:
+        return "Artifacts"
+    if "enchantment" in type_line:
+        return "Enchantments"
+    if "land" in type_line:
+        return "Lands"
+    return "Other"
+
+
+def get_card_metadata(card_name):
+    try:
+        card = scrython.cards.Named(fuzzy=card_name)
+        card_type = card.type_line()
+        card_mana_cost = card.mana_cost().replace('{', '').replace('}', '')
+        real_type = type_line_to_type(card_type)
+
+        return (card_name, real_type, card_mana_cost)
+    except Exception as e:
+        print(e)
+        return (card_name, "Other", "?")
+
+
+def insert_decklist(metadata):
+    (event_date, winner, deckname, decklist_filename) = metadata
+
+    print("Inserting decklist %s for date %s" % (decklist_filename, event_date))
+
+    with open(decklist_filename, 'r') as f:
+        lines = f.readlines()
+        rows = []
+        for line in lines:
+            line = line.strip('\n')
+            if len(line) == 0:
+                continue
+            (card_count, separator, card_name) = line.strip('\n').partition(' ')
+            print(card_name)
+
+            (_, card_type, card_mana_cost) = get_card_metadata(card_name)
+
+            rows.append((event_date, int(card_count), card_name, card_type, card_mana_cost))
+
+        c.executemany(
+            'INSERT INTO event_decklist (event_date, card_qty, card_name, card_type, card_mana_cost) VALUES (?,?,?,?,?)',
+            rows)
+
+
+def insert_event(metadata):
+    (event_date, winner, deckname, decklist_filename) = metadata
+    print("Inserting event for date " + event_date)
+    rows = [(event_date, winner, deckname)]
+    c.executemany('INSERT INTO event (event_date, winner, deckname) VALUES (?,?,?)', rows)
+
+
+def get_events():
+    c.execute("select * from event")
+    return c.fetchall()
+
+
+def get_event(event_date):
+    c.execute("select * from event where event_date = ?", (event_date,))
+    return c.fetchone()
+
+
+def get_decklist(event_date):
+    c.execute("select * from event_decklist where event_date = ?", (event_date,))
+    return c.fetchall()
+
+
+def insert():
+    for event in event_metadata:
+        insert_event(event)
+        insert_decklist(event)
+
+    conn.commit()
+
+
+if __name__ == '__main__':
+    event_metadata = [
+        ('2019-05-20', 'Sean', 'UW Control', 'data/decklists/2019-05-20_Cube_Winner_Sean_-_UW_Control.txt'),
+        ('2019-08-03', 'Phil', 'UW Control', 'data/decklists/2019-08-03_Cube_Winner_Phil_-_UW_Control.txt'),
+        ('2019-08-31-draft1', 'Phil', 'UB Artifacts', 'data/decklists/2019-08-31_Cube_Winner_Phil_-_UB_Artifacts.txt'),
+        ('2019-08-31-draft2', 'Phil', 'UW Artifacts', 'data/decklists/2019-08-31_Cube_Winner_Phil_UW_Artifacts.txt'),
+        ('2019-12-20', 'Brian', 'UWr Control Twin', 'data/decklists/2019-12-20_Cube_Winner_Brian_-_URw_Control_Twin.txt'),
+        ('2020-01-04', 'Matt', 'Oath Ramp', 'data/decklists/2020-01-04_Cube_Winner_Matt_-_Oath_Ramp.txt'),
+        ('2020-02-01-draft1', 'Brian', 'Mono Green', 'data/decklists/2020-02-01_Cube_Winner_1_Brian_-_Mono_Green.txt'),
+        ('2020-02-01-draft2', 'Jeff', 'Mono Red', 'data/decklists/2020-02-01_Cube_Winner_2_Jeff_-_Mono_Red.txt')
+    ]
+
+    match_results = [
+        ('')
+    ]
+
+    init()
+    # insert()
+
+    conn.commit()
+    conn.close()
